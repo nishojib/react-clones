0318efb6257e87e0f640be55202fc74aa98a6817
Nurul Islam <nishojib@gmail.com>
2023-04-19 19:06:34 -0400

    feat:added navbar component

--- apps/reddit-clone/tailwind.config.js
+++ apps/reddit-clone/tailwind.config.js
@@ -1,7 +1,7 @@
 const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
 const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
     join(
@@ -12,6 +12,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    keyframes: {
+      slideDown: {
+        from: { height: 0 },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      slideUp: {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: 0 },
+      },
+    },
+    animation: {
+      slideDown: 'slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)',
+      slideUp: 'slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)',
+    },
   },
   plugins: [],
 };
